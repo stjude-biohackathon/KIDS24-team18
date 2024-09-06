5aed1bcfadf4b32f4dae887bda83517ea15f6ead
sdhutchins <sdhutchins@uab.edu>
2024-09-06 15:40:24 -0500

    Add dpk example

---
+++ examples/run_grumpy_dpk_openai.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define variables for paths and parameters
+GRUMPY_SCRIPT=grumpy
+# Update the API Key File Path
+API_KEY_FILE=/Users/wqi65/Documents/BioHack/KIDS24_Grumpy_API
+API_TYPE=openai
+MODEL_NAME=gpt-4o-mini #choose from gpt-4o, gpt-4o-mini, llama3, meditron, medllama2
+
+INPUT_FILE_2="./examples/DPK_examples"
+OUTFILES_PREFIX_2="GSE247821"
+CONTEXT_FILE_2="./examples/DPK_examples/biological_context_example"
+PROTOCOL_2="atacseq"
+MODE="DPK"
+
+# Run Grumpy on the updated dataset
+$GRUMPY_SCRIPT \
+$MODE \
+-i $INPUT_FILE_2 \
+--outfilesPrefix $OUTFILES_PREFIX_2 \
+--context $CONTEXT_FILE_2 \
+-p $PROTOCOL_2 \
+-k $API_KEY_FILE \
+--apiType $API_TYPE \
+--gptModel $MODEL_NAME
