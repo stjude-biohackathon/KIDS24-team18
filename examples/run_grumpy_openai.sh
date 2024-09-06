8a37fd977328880a0f0c87a1fc62a484e171c662
sdhutchins <sdhutchins@uab.edu>
2024-09-05 21:33:37 -0500

    Fixed example for openai.

--- examples/run_grumpy_openai.sh
+++ examples/run_grumpy_openai.sh
@@ -8,13 +8,15 @@ API_TYPE=openai
 MODEL_NAME=gpt-4o #choose from gpt-4o, gpt-4o-mini, llama3, meditron, medllama2
 
 INPUT_FILE_2="./examples/RNK_GSE164073_diff.sclera_CoV2_vs_sclera_mock.regulation.rank_/GMT_h.all.v2023.2.Hs.symbols"
-OUTFILES_PREFIX_2="GSE164073_sclera"
-CONTEXT_FILE_2="context.txt"
+OUTFILES_PREFIX_2="./examples/GSE164073_sclera"
+CONTEXT_FILE_2="./examples/context.txt"
 PROTOCOL_2="gsea"
 REPORT_2="gseareport"
+MODE="PE"
 
 # Run Grumpy on a test dataset
 python $GRUMPY_SCRIPT \
+$MODE \
 -i $INPUT_FILE_2 \
 --outfilesPrefix $OUTFILES_PREFIX_2 \
 --context $CONTEXT_FILE_2 \
